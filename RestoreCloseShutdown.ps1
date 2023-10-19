9662d502e9876a82c7df74d14857d88081d037c3
Jay <52718104+ProtahJay@users.noreply.github.com>
2023-10-18 20:35:43 -0400

    Update RestoreCloseShutdown.ps1

--- RestoreCloseShutdown.ps1
+++ RestoreCloseShutdown.ps1
@@ -24,50 +24,65 @@ Add-Type -TypeDefinition @"
 $SW_RESTORE = 9
 
 # -----------------------------------------------------------------------------
-# STEP 3: Fetch all running processes that have a MainWindowTitle.
+# STEP 3: Gracefully close all instances of Elgato Wave Link app if they're running.
+# -----------------------------------------------------------------------------
+$waveLinkProcesses = Get-Process | Where-Object { $_.ProcessName -eq "WaveLink" -and $_.Id -ne $PID }
+
+foreach ($process in $waveLinkProcesses) {
+    try {
+        # 3.1: Use the 'ShowWindow' function to restore the application window if it's minimized.
+        [void][User32]::ShowWindow($process.MainWindowHandle, $SW_RESTORE)
+
+        # 3.2: Pause for a short duration to ensure the window has time to restore.
+        Start-Sleep -Milliseconds 500
+
+        # 3.3: Attempt to close the Wave Link application gracefully.
+        $process.CloseMainWindow()
+
+        # 3.4: Pause again to allow the application to process the close request.
+        Start-Sleep -Seconds 1
+    } catch {
+        # 3.5: Handle any exceptions that might occur during the process.
+        Write-Output "Error handling WaveLink process: $_"
+    }
+}
+
+# -----------------------------------------------------------------------------
+# STEP 4: Fetch all running processes that have a MainWindowTitle.
 # This filters out background processes or those without a GUI.
 # Additionally, we ensure we don't target the current PowerShell process.
 # -----------------------------------------------------------------------------
 $runningProcesses = Get-Process | Where-Object { $_.MainWindowTitle -ne '' -and $_.Id -ne $PID }
 
 # -----------------------------------------------------------------------------
-# STEP 4: Iterate over each identified process.
+# STEP 5: Iterate over each identified process.
 # -----------------------------------------------------------------------------
 foreach ($process in $runningProcesses) {
     try {
-        # 4.1: Use the 'ShowWindow' function to restore the application window
-        # if it's minimized.
+        # 5.1: Use the 'ShowWindow' function to restore the application window if it's minimized.
         [void][User32]::ShowWindow($process.MainWindowHandle, $SW_RESTORE)
 
-        # 4.2: Pause for a short duration to ensure the window has time to restore.
+        # 5.2: Pause for a short duration to ensure the window has time to restore.
         Start-Sleep -Milliseconds 500
 
-        # 4.3: Attempt to close the application gracefully using its main window.
-        # This sends a 'close' signal to the application, allowing it to prompt 
-        # the user or shut down properly.
+        # 5.3: Attempt to close the application gracefully using its main window.
         $process.CloseMainWindow()
 
-        # 4.4: Pause again to allow the application to process the close request.
+        # 5.4: Pause again to allow the application to process the close request.
         Start-Sleep -Seconds 1
-
-        # Note: At this point, if the application is still running, you might
-        # consider using more aggressive means like 'taskkill' or 'Force Close' to
-        # ensure it's shut down. This step is optional based on user preference.
     } catch {
-        # 4.5: Handle any exceptions that might occur during the process.
-        # This might happen if, for instance, an application closes before the 
-        # script has a chance to perform an action on it.
+        # 5.5: Handle any exceptions that might occur during the process.
         Write-Output "Error handling process $($process.Name): $_"
     }
 }
 
 # -----------------------------------------------------------------------------
-# STEP 5: Give all applications some additional time to close gracefully.
+# STEP 6: Give all applications some additional time to close gracefully.
 # -----------------------------------------------------------------------------
 Start-Sleep -Seconds 2
 
 # -----------------------------------------------------------------------------
-# STEP 6: Initiate the shutdown of the Windows computer.
+# STEP 7: Initiate the shutdown of the Windows computer.
 # This will forcefully shut down the machine, ensuring all operations cease.
 # -----------------------------------------------------------------------------
 Stop-Computer -Force
